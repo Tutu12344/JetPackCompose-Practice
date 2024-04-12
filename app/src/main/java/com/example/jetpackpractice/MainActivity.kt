110158ca995769d383d2b074e26d1acabb1f33a6
Tutu12344 <padde.cross@gmail.com>
2024-04-12 22:43:18 +0900

    happy birthday card complete

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.jetpackpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +33,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText("Happy Birthday Sam!", from = "From Emma",modifier = Modifier.padding(8.dp))
+                    GreetingImage(stringResource(R.string.happy_birthday_text), from = stringResource(
+                        R.string.signature_text
+                    ),modifier = Modifier.padding(8.dp))
                 }
             }
         }
@@ -52,15 +58,32 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
 
+@Composable
+fun GreetingImage(message: String,from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier){
+        //アスペクト比を維持しながら均等に画像を拡大
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop, alpha = 0.5F)
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthDayCardPreview() {
     JetpackpracticeTheme {
-        GreetingText("Happy Birthday Sam!", from = "From Emma")
+        GreetingImage("Happy Birthday Sam!", from = "From Emma")
     }
 }
+
