2990cbd97203a5479aab2c6b4a1a97d3d6537a13
Tutu12344 <padde.cross@gmail.com>
2024-04-12 23:03:03 +0900

    News Layout create

--- app/src/main/java/com/example/jetpackpractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackpractice/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,12 +11,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,9 +28,13 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(stringResource(R.string.happy_birthday_text), from = stringResource(
-                        R.string.signature_text
-                    ),modifier = Modifier.padding(8.dp))
+                    NewsUI(
+                        title = stringResource(id = R.string.title_text),
+                        subTitle = stringResource(id = R.string.subtitle_text),
+                        mainText = stringResource(
+                            id = R.string.main_text
+                        )
+                    )
                 }
             }
         }
@@ -43,47 +42,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column (
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier){
+fun NewsUI(title: String, subTitle: String, mainText: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(
+        modifier = modifier
+    ) {
+        Image(painter = image, contentDescription = null)
         Text(
-            text = message,
-            fontSize =100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            text = title,
+            modifier = Modifier
+                .padding(16.dp),
+            fontSize = 24.sp,
         )
         Text(
-            text = from,
-            fontSize = 36.sp,
+            text = subTitle,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = mainText,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
 
-@Composable
-fun GreetingImage(message: String,from: String, modifier: Modifier = Modifier){
-    val image = painterResource(R.drawable.androidparty)
-    Box(modifier){
-        //アスペクト比を維持しながら均等に画像を拡大
-        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop, alpha = 0.5F)
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
-    }
-
-}
 @Preview(showBackground = true)
 @Composable
-fun BirthDayCardPreview() {
+fun NewsCardPreview() {
     JetpackpracticeTheme {
-        GreetingImage("Happy Birthday Sam!", from = "From Emma")
+        NewsUI(
+            title = stringResource(id = R.string.title_text),
+            subTitle = stringResource(id = R.string.subtitle_text),
+            mainText = stringResource(
+                id = R.string.main_text
+            )
+        )
     }
 }
 
